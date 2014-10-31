255978e86935dfb02998c96e5e9249edc60beb8a
Pablo Seibelt <pabloseibelt@gmail.com>
2014-10-31 01:43:10 -0300

    Added android platform

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/sicarul/dev/bluelytics_mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/sicarul/dev/bluelytics_mobile/platforms/android/CordovaLib/AndroidManifest.xml \
