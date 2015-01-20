b93bc01a2febd60c145d05b0998e2237cd3a7aef
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2015-01-20 01:35:42 -0300

    Version bump 1.1.0

--- finalbuild.sh
+++ finalbuild.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cordova build --release android && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../mobile_release.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk bluekey && /home/sicarul/android-sdk-linux/build-tools/21.0.2/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/Bluelytics.apk
+rm platforms/android/ant-build/Bluelytics.apk
+cordova build --release android && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../mobile_release.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk bluekey && /home/sicarul/adt-bundle-linux-x86_64-20140702/sdk/build-tools/android-4.4W/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/Bluelytics.apk
