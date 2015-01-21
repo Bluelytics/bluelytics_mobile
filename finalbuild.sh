e61af0d8933dec7ddcc90c8fd0616ef802d980de
Pablo Seibelt <pabloseibelt@gmail.com>
2015-01-21 15:14:51 -0300

    Changed gulp to uglify

--- finalbuild.sh
+++ finalbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm platforms/android/ant-build/Bluelytics.apk
-cordova build --release android && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../mobile_release.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk bluekey && /home/sicarul/adt-bundle-linux-x86_64-20140702/sdk/build-tools/android-4.4W/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/Bluelytics.apk
+cordova build --release android && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../mobile_release.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk bluekey && zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/Bluelytics.apk
